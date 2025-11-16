fb458ccf988ccd78182bb48b1f42be44a5968efd
Tanisha-gitcodes <stanishams@gmail.com>
2025-11-16 22:43:41 +0000

    Update with Enum

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from enum import Enum
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "AlexNet model"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+    elif model_name == ModelName.resnet:
+        return {"model_name": model_name, "message": "ResNet model"}
+
+    return {"model_name": model_name, "message": "Have some residual"}
